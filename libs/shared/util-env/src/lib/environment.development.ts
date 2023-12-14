2c51b76f22fffcf0775c2ce7e3c729acaf29daf4
Lars Hoendervangers <lcchoendervangers@outlook.com>
2023-12-14 11:38:45 +0100

    Added: dotenv package.

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -1,11 +1,14 @@
 import { IEnvironment } from './environment.interface';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const environment: IEnvironment = {
   production: false,
   backendUrl: 'http://localhost:3000',
   neo4j: {
     username: 'neo4j',
-    password: '',
+    password: process.env.GRAPH_PASSWORD,
     scheme: 'neo4j',
     host: '07b40077.databases.neo4j.io',
     database: 'iHomer',
--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -1,11 +1,14 @@
 import { IEnvironment } from './environment.interface';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const environment: IEnvironment = {
   production: true,
   backendUrl: 'https://iconnected.azurewebsites.net',
   neo4j: {
     username: 'neo4j',
-    password: '',
+    password: process.env.GRAPH_PASSWORD,
     scheme: 'neo4j',
     host: '07b40077.databases.neo4j.io',
     database: 'iHomer',
