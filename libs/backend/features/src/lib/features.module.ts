03e30571aefa274524a97d3e10970ac1ffaf18da
Lars Hoendervangers <lcchoendervangers@outlook.com>
2023-12-19 12:04:15 +0100

    Added: Neo4j dependencies.

--- libs/backend/features/src/lib/features.module.ts
+++ libs/backend/features/src/lib/features.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { environment } from '@ihomer/shared/util-env';
+import { Neo4jModule, Neo4jScheme } from "nest-neo4j/dist";
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule, 
+    Neo4jModule.forRoot({
+        scheme: environment.neo4j.scheme as Neo4jScheme,
+        host: environment.neo4j.host,
+        password: environment.neo4j.password,
+        username: environment.neo4j.username,
+        database: environment.neo4j.database,
+        port: environment.neo4j.port
+    })
+  ],
   controllers: [],
   providers: [],
   exports: [],
--- libs/backend/features/src/lib/user/user.service.ts
+++ libs/backend/features/src/lib/user/user.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { Neo4jService } from "nest-neo4j/dist";
 
 @Injectable()
 export class UserService {
+    TAG = 'UserService';
     
+    constructor(private readonly neo4jService: Neo4jService) {}
+
+    create(): void {
+        // this.neo4jService.write('')
+    }
 }
