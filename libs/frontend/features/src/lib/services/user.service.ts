eaf2b3f928c9ea26ba878f444b9b7ba9d12d913c
Emma van den Broek <emma30121998@gmail.com>
2023-12-21 15:14:49 +0100

    userservice gettags

--- libs/frontend/features/src/lib/services/user.service.ts
+++ libs/frontend/features/src/lib/services/user.service.ts
@@ -3,6 +3,7 @@ import { EntityService } from './entity.service';
 import { IUser } from '@ihomer/shared/api';
 import { NotificationService } from './notifications/notification.service';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -12,5 +13,10 @@ export class UserService extends EntityService<IUser> {
     constructor(http: HttpClient, notificationService: NotificationService
     ) {
         super(http, 'http://localhost:3000/api', 'users', notificationService);
+    }
+
+    getDistinctTagsForAllUsers(): Observable<string[]> {
+        return this.http
+        .get<string[]>('http://localhost:3000/api/tags');
     }
 }
