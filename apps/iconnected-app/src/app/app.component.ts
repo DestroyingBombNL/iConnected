8f3ba1adc003eac0b801a9b6da7bc9bc3340ed3f
Lars Hoendervangers <lcchoendervangers@outlook.com>
2023-12-22 12:54:44 +0100

    Added: Page titles.

--- apps/iconnected-app/src/app/app.component.ts
+++ apps/iconnected-app/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterLink, RouterOutlet } from '@angular/router';
 import { FrontendFeaturesModule } from '@ihomer/frontend/features';
 import { UiModule } from '@ihomer/frontend/ui';
-import { frontendEnvironment } from '@ihomer/shared/util-env';
 
 @Component({
   standalone: true,
@@ -12,10 +11,8 @@ import { frontendEnvironment } from '@ihomer/shared/util-env';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'iconnected-app';
-  imagePath?: string;
+  title = 'iConnected';
+
   constructor() {
-    this.imagePath = 'assets/logoiHomer.png';
-    console.log(frontendEnvironment)
   }
 }
