84c375090af95dde5baf7ddb6fed9f4a08441351
Lars Hoendervangers <lcchoendervangers@outlook.com>
2023-12-22 10:21:21 +0100

    Fixed: Missing component errors.

--- libs/frontend/ui/src/lib/ui.module.ts
+++ libs/frontend/ui/src/lib/ui.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterLink } from '@angular/router';
-import { BendeOverviewComponent } from './bendes/overview/overview.component';
 
 @NgModule({
   imports: [CommonModule, RouterLink],
-  declarations: [HeaderComponent, BendeOverviewComponent],
-  exports: [HeaderComponent, BendeOverviewComponent],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
 })
 export class UiModule {}
