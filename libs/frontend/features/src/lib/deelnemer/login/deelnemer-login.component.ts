d3c2ba38d97a420fb011c7907cbe219b7006bde9
Sven <Svenhu@live.nl>
2024-01-08 13:54:48 +0100

    Add: User can be logged in

--- libs/frontend/features/src/lib/deelnemer/login/deelnemer-login.component.ts
+++ libs/frontend/features/src/lib/deelnemer/login/deelnemer-login.component.ts
@@ -26,6 +26,7 @@ export class LoginComponent implements OnInit, OnDestroy {
       email: new FormControl("", [ Validators.required, this.validEmail.bind(this) ]),
       password: new FormControl("", [ Validators.required, this.validPassword.bind(this) ]),
     });
+    console.log(this.authService.getUserFromLocalStorage());
     /*
     if (this.authService.getUserFromLocalStorage()) {
       this.router.navigate(['/']);
