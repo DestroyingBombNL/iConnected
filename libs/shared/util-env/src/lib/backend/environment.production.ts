bc1c7fdf9c7136ba7d90aae4b7d826dcdd8f4cb8
Lars Hoendervangers <lcchoendervangers@outlook.com>
2024-01-09 08:50:43 +0100

    Edit: Env for graph password.

--- libs/shared/util-env/src/lib/backend/environment.production.ts
+++ libs/shared/util-env/src/lib/backend/environment.production.ts
@@ -4,7 +4,7 @@ export const backendEnvironment: IEnvironment = {
   production: true,
   neo4j: {
     username: 'neo4j',
-    password: process.env["GRAPH_PASSWORD"] ?? "",
+    password: process.env.GRAPH_PASSWORD,
     scheme: 'neo4j',
     host: '07b40077.databases.neo4j.io',
     database: 'iHomer',
