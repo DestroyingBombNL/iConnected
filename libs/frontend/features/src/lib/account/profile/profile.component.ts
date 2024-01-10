a5c53b1de03b4b3897d9256e3a637e442c735f7e
Lars Hoendervangers <lcchoendervangers@outlook.com>
2024-01-10 14:58:51 +0100

    Removed: Preset login data.

--- libs/frontend/features/src/lib/account/profile/profile.component.ts
+++ libs/frontend/features/src/lib/account/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { AuthService } from '../../auth/auth.service';
