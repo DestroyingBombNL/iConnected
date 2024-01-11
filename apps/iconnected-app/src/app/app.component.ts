dc5583f376627031fa2ad4e7ad417f258e42dc33
Kenanvdh <kenanvdh@ziggo.nl>
2024-01-11 10:09:14 +0100

    Dangerous toast styling

--- apps/iconnected-app/src/app/app.component.ts
+++ apps/iconnected-app/src/app/app.component.ts
@@ -2,12 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { INotification, NotificationType } from '@ihomer/api';
-import { FrontendFeaturesModule, NotificationService } from '@ihomer/frontend/features';
+import {
+  FrontendFeaturesModule,
+  NotificationService,
+} from '@ihomer/frontend/features';
 import { UiModule } from '@ihomer/frontend/ui';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, RouterLink, UiModule, FrontendFeaturesModule, CommonModule],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    UiModule,
+    FrontendFeaturesModule,
+    CommonModule,
+  ],
   selector: 'ihomer-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
@@ -16,18 +25,12 @@ export class AppComponent {
   protected activeNotification$?: INotification | undefined;
 
   constructor(readonly notificationService: NotificationService) {
-    this.activeNotification$ = {
-      details: 'Details van foutmelding hier.',
-      duration: 5000,
-      title: 'Titel van foutmelding',
-      type: NotificationType.Success
-    }
-    // this.notificationService.notification$.subscribe((notification) => {
-    //   this.activeNotification$ = notification;
-    //   setTimeout(() => {
-    //     this.activeNotification$ = undefined;
-    //   }, notification?.duration);
-    // });
+    this.notificationService.notification$.subscribe((notification) => {
+      this.activeNotification$ = notification;
+      setTimeout(() => {
+        this.activeNotification$ = undefined;
+      }, notification?.duration);
+    });
   }
 
   getNotificationColor(notification?: INotification): string {
@@ -42,5 +45,9 @@ export class AppComponent {
       default:
         return '';
     }
+  }
+
+  deactivateNotification(): void {
+    this.activeNotification$ = undefined;
   }
 }
